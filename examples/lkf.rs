b8601e628cc5027d069bbe9b0d0bb46db43e9981
Niranjan Ravichandra <nravic@protonmail.ch>
2019-12-25 21:58:18 -0500

    Added example file. Need to take care of impl outside of crate issue.

---
+++ examples/lkf.rs
@@ -0,0 +1,35 @@
+ // examples/lkf.rs
+// Simple example for instantiating and using a Linear Kalman Filter
+extern crate kalmanrs as krs;
+extern crate nalgebra as na;  
+use krs::{KalmanState, LinearKalman};
+use na::{U3, Matrix3, Vector3};
+
+krs::lkf_builder! {f64, U3, U3, U3}
+
+fn main() {
+    let mut example_kalman = LinearKalman {
+        Q: Matrix3::new_random(),
+        A: Matrix3::new_random(),
+        B: Matrix3::new_random(),
+        P: Matrix3::new_random(),
+        K: Matrix3::new_random(),
+        R: Matrix3::new_random(),
+        H: Matrix3::new_random(),
+        I: Matrix3::new_random()
+    };
+ 
+    let mut kalman_state = KalmanState {
+        x: Vector3::new_random(),
+        u: Vector3::new_random(),
+        z: Vector3::new_random()
+    }; 
+  
+    let _dt = 0.1;
+    let t_max = 30;
+    for _t in (0..t_max).step_by(1) {
+        example_kalman.predict(&mut kalman_state);
+        example_kalman.update(&mut kalman_state);
+        println!("{:?}", kalman_state)
+    };
+}
