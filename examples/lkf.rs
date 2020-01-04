35ed84422be1c16e36723c9f6892bdbd6ce7eb8a
Niranjan Ravichandra <nravic@protonmail.ch>
2020-01-03 21:31:54 -0500

    Figured out how to impl the LKF struct

--- examples/lkf.rs
+++ examples/lkf.rs
@@ -2,13 +2,18 @@
 // Simple example for instantiating and using a Linear Kalman Filter
 extern crate kalmanrs as krs;
 extern crate nalgebra as na;   
-use krs::{KalmanState, LinearKalman, lkf_tuple};
+use krs::{KalmanState, LinearKalman, lkf_builder};
 use na::{U3, Matrix3, Vector3};
 
-lkf_tuple! {KalmanState, LinearKalman, f64, U3, U3, U3};
+struct KalmanWrapper {
+    lk: LinearKalman<f64, U3, U3, U3>, 
+    state: KalmanState<f64, U3, U3, U3>
+}
+ 
+lkf_builder! {KalmanWrapper}
 
 fn main() {
-    let mut example_kalman = LinearKalman {
+    let example_kalman = LinearKalman {
         Q: Matrix3::new_random(),
         A: Matrix3::new_random(),
         B: Matrix3::new_random(),
@@ -19,18 +24,22 @@ fn main() {
         I: Matrix3::new_random()
     };
  
-    let mut kalman_state = KalmanState {
+    let kalman_state = KalmanState {
         x: Vector3::new_random(),
         u: Vector3::new_random(),
         z: Vector3::new_random()                            
     };
-    let mut filter = Filter(example_kalman, kalman_state);
-  
-    let _dt = 0.1;
+
+    let mut filter = KalmanWrapper {
+        lk: example_kalman,
+        state: kalman_state
+    };
+
+    let _dt = 0.1; 
     let t_max = 30;
     for _t in (0..t_max).step_by(1) {
-        example_kalman.predict(&mut kalman_state);
-        example_kalman.update(&mut kalman_state);
-        println!("{:?}", kalman_state)
+        filter.predict();
+        filter.update();
+        println!("{:?}", filter.state)
     };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -23,9 +23,13 @@ where DefaultAllocator: Allocator<T, D1, D1> // allocator for nxn matrix
     pub I: MatrixMN<T, D1, D1>, // Identity matrix. Stored because of ndarray generic shenanigans
 }
 
-// #[derive(Debug)]
-// struct UnscentedKalman<T: Scalar, D: Dim>
-// where DefaultAllocator: Allocator<T, D, D> {}
+#[derive(Debug)]
+pub struct UnscentedKalman<T: Scalar, D1: Dim, D2: Dim, D3: Dim> // (d1: n, d2: m, d3: l)
+where DefaultAllocator: Allocator<T, D1, D1> // allocator for nxn matrix
+    + Allocator<T, D2, D1> // allocator for (mxn) matrix
+    + Allocator<T, D1, D2> // allocator for (nxm) matrix 
+    + Allocator<T, D1, D3> { // allocator for (nxl) matrix  
+}
 
 #[derive(Debug)]
 pub struct KalmanState<T: Scalar, D1: Dim, D2: Dim, D3: Dim>
@@ -38,40 +42,31 @@ where DefaultAllocator: Allocator<T, D1>
 }
 
 // -- Macro to build the Linear Kalman Filter -- 
-// Because the macro implements a generic type, and the impl isn't defined in this file, the user has to define the implementation. An issue then arises because in Rust it is not possible to impl a type outside of where it is defined.
-// $lkf_struct and $lkf_state hence are structs that need to be created by the user that respectively encapsulate the LinearKalman and KalmanState structs.
-
-#[macro_export]
-    macro_rules! lkf_tuple {
-    ($lkf_struct: ty, $lkf_state: ty, $scalar: ty, $dim_n: ty, $dim_m: ty, $dim_l: ty) => {
-        struct Filter($lkf_struct<$scalar, $dim_n, $dim_m, $dim_l>,
-            $lkf_state<$scalar, $dim_n, $dim_m, $dim_l>);
-    }
-}
+// Because the macro implements a generic type, and the impl isn't defined in this file, the user has to define the implementation. An issue then arises because in Rust it is not possible to impl a type outside the crate where it is defined.
+// $lkf_wrapper is hence a struct that need to be created by the user that encapsulates the LinearKalman and KalmanState structs.
 
 #[macro_export]
 macro_rules! lkf_builder {
-    ($lkf_struct: ty, $lkf_state: ty, $scalar: ty, $dim_n: ty, $dim_m: ty, $dim_l: ty) => {
-        impl $lkf_struct<$scalar, $dim_n, $dim_m, $dim_l> {
-                
-            fn predict(&mut self, state: &mut $kstate<$scalar, $dim_n, $dim_m, $dim_l>) {
+    ($lkf_wrapper: ty) => {
+        impl $lkf_wrapper {                
+            fn predict(&mut self) {
                 // projects state ahead 
-               state.x = self.A * state.x + self.B*state.u;
+               self.state.x = self.lk.A * self.state.x + self.lk.B*self.state.u;
                 // project error covariance ahead
-                self.P = self.A * self.P * self.A.transpose() + self.Q;
+                self.lk.P = self.lk.A * self.lk.P * self.lk.A.transpose() + self.lk.Q;
             }
             
-            fn update(&mut self, state: &mut $kstate<$scalar, $dim_n, $dim_m, $dim_l>) {
+             fn update(&mut self) {
                 // compute Kalman gain
-                let mut inner_val = self.H * self.P * self.H.transpose() + self.R;
+                let mut inner_val = self.lk.H * self.lk.P * self.lk.H.transpose() + self.lk.R;
                 inner_val.try_inverse_mut();
-                self.K = self.P * self.H.transpose() * inner_val;
+                self.lk.K = self.lk.P * self.lk.H.transpose() * inner_val;
 
                 // update estimate with measurement z
-                state.x = state.x + self.K*(state.z - self.H * state.x);
+                self.state.x = self.state.x + self.lk.K*(self.state.z - self.lk.H * self.state.x);
 
                 // update error covariance
-                self.P = (self.I - self.K * self.H) * self.P;
+                self.lk.P = (self.lk.I - self.lk.K * self.lk.H) * self.lk.P;
             }
         }
     }
